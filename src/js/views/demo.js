d9ad367cf1c12979762b2d716ecf1b60a414da44
Monique Menezes <122538719+Niquemenezes@users.noreply.github.com>
2025-01-31 19:11:50 +0000

    lista de contactos

--- src/js/views/demo.js
+++ src/js/views/demo.js
@@ -1,43 +1,56 @@
-import React, { useState, useEffect, useContext } from "react";
-import { Link } from "react-router-dom";
-
+import React, { useEffect, useContext } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPen, faTrashCan, faPhone, faLocationDot, faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import { Context } from "../store/appContext";
-
 import "../../styles/demo.css";
 
-export const Demo = () => {
-	const { store, actions } = useContext(Context);
 
-	return (
-		<div className="container">
-			<ul className="list-group">
-				{store.demo.map((item, index) => {
-					return (
-						<li
-							key={index}
-							className="list-group-item d-flex justify-content-between"
-							style={{ background: item.background }}>
-							<Link to={"/single/" + index}>
-								<span>Link to: {item.title}</span>
+	export const Demo = () => {
+		const { store, actions } = useContext(Context);
+		const navigate = useNavigate();
+	
+			return (
+				<div className="container">
+					<nav className="navbar  mb-3">
+						<div className="d-flex justify-content-end w-100">
+							<Link to="/">
+								<button className="btn btn-success">Add new contact</button>
 							</Link>
-							{// Conditional render example
-							// Check to see if the background is orange, if so, display the message
-							item.background === "orange" ? (
-								<p style={{ color: item.initial }}>
-									Check store/flux.js scroll to the actions to see the code
-								</p>
-							) : null}
-							<button className="btn btn-success" onClick={() => actions.changeColor(index, "orange")}>
-								Change Color
-							</button>
-						</li>
-					);
-				})}
+						</div>
+					</nav>
+
+				<ul className="list-group">
+				{Array.isArray(store.contacts) && store.contacts.length > 0 ? (
+					store.contacts.map((item, index) => {
+						return (
+							<li key={index} className="list-group-item d-flex justify-content-between ">
+								<div className="d-flex align-items-center">
+									<img src="https://picsum.photos/id/64/150/150" 	alt="Contact" className="img-circle"/>
+									<div className="datos-del-contacto ml-3">
+										<h3>{item.name}</h3> 
+										<p>
+											<FontAwesomeIcon icon={faLocationDot} style={{color: "#646464", marginRight: "10px"}} />
+											{item.address}
+											<br/>
+											<FontAwesomeIcon icon={faPhone} flip="horizontal" style={{color: "#646464", marginRight: "10px"}} />
+											{item.phone}
+											<br/>
+											<FontAwesomeIcon icon={faEnvelope} style={{color: "#646464", marginRight: "10px"}} />
+											{item.email}
+										</p>
+									</div>
+								</div>
+								<div className="iconos">
+								<FontAwesomeIcon icon={faPen} onClick={()=> navigate(`/edit/${item.id}`)} className="m-3"/>
+								<FontAwesomeIcon icon={faTrashCan} onClick={()=>actions.deleteContact(item.id)} className="m-3"/>
+								</div>
+							</li>
+						);
+					})
+				) : (<p>No contacts found.</p> // Muestra un mensaje si no hay contactos
+				)}
 			</ul>
-			<br />
-			<Link to="/">
-				<button className="btn btn-primary">Back home</button>
-			</Link>
 		</div>
 	);
 };
