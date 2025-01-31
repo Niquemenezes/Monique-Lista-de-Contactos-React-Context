2f86ca406167a49dc86c1dd00c65191fa84926c2
Monique Menezes <122538719+Niquemenezes@users.noreply.github.com>
2025-01-31 17:13:35 +0000

    lista de contactos

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,45 +1,122 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
+			contacts: []
 		},
 		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
+			
+			// Obtener contactos
+			obtenerContacts: () => {
+				fetch('https://playground.4geeks.com/contact/agendas/Monique/contacts', {
+					method: "GET",
+					headers: { "Content-Type": "application/json" },
+				})
+				.then((response) => {
+					console.log("API response:", response);
+					if (!response.ok) {
+						return response.text().then(text => { throw new Error(`Error: ${response.status} - ${text}`) });
+					}
+					return response.json();
+				})
+				.then((data) => {
+					console.log("Datos recibidos de la API:", data);
+					if (Array.isArray(data.contacts)) {
+						setStore({ contacts: data.contacts });
+					} else {
+						setStore({ contacts: [] });
+					}
+				})
+				.catch(error => console.error("Error procesando datos:", error));
 			},
-			loadSomeData: () => {
-				/**
-					fetch().then().then(data => setStore({ "foo": data.bar }))
-				*/
-			},
-			changeColor: (index, color) => {
-				//get the store
+			
+
+			// Agregar contacto al store
+			addContactToContacts: (contact) => {
 				const store = getStore();
+				setStore({ contacts: [...store.contacts, contact] });
+			},
 
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
+			// Crear contacto
+			createContact: (payload) => {
+				fetch("https://playground.4geeks.com/contact/agendas/Monique/contacts", {
+					method: "POST",
+					headers: { "Content-Type": "application/json" },
+					body: JSON.stringify(payload),
+				})
+				.then((response) => {
+					if (!response.ok) {
+						throw new Error("Error al crear el contacto");
+					}
+					return response.json();
+				})
+				.then((data) => {
+					const actions = getActions();
+					actions.addContactToContacts(data);
+					console.log("Contacto agregado:", data);
+				})
+				.then(() => {
+					getActions().obtenerContacts();
+				})
+				
+			},
 
-				//reset the global store
-				setStore({ demo: demo });
-			}
+			// Eliminar contacto
+			deleteContact: (id) => {
+				console.log("Intentando eliminar el contacto con ID:",id);
+				//Eliminacion del contacto en la API
+				fetch(`https://playground.4geeks.com/contact/agendas/Monique/contacts/${id}`, {
+					method: "DELETE",
+					headers: { "Content-Type": "application/json" }
+				})
+				.then((response) => {
+					if (!response.ok) {
+						throw new Error("Error al eliminar el contacto");
+					}
+					 // Verifica si la respuesta tiene cuerpo
+					 if (response.status === 204) {
+						console.log(`Contacto con ID ${id} eliminado correctamente`);
+					  } else {
+					return response.json();
+					  }
+				})
+				.then(() => {
+					//actualizar el store local para eliminar el contacto de la lista
+					const store = getStore();
+					const updatedContacts = store.contacts.filter(contact => contact.id !== id);
+					setStore({ contacts: updatedContacts });
+					console.log(`Contacto con ID ${id} eliminado`);
+				})
+				.then(() => {
+					// Después de eliminarlo localmente, actualiza los contactos desde la API
+					getActions().obtenerContacts();
+				  });
+				
+			},
+
+			// Editar contacto
+			changeContact: (id, updatedContact) => {
+				fetch(`https://playground.4geeks.com/contact/agendas/Monique/contacts/${id}`, {
+					method: "PUT",
+					headers: { "Content-Type": "application/json" },
+					body: JSON.stringify(updatedContact)
+				})
+				.then((response) => {
+					if (!response.ok) {
+						throw new Error("Error al actualizar el contacto");
+					}
+					console.log(`Contacto con ID ${id} actualizado`);
+					//  Volvemos a cargar los contactos para asegurar que estén actualizados
+					return fetch("https://playground.4geeks.com/contact/agendas/Monique/contacts");
+				})
+				.then(response => response.json())
+				.then(data => {
+					setStore({ contacts: data.contacts });
+				})
+				.catch((error) => console.error("Error:", error));
+			},
 		}
 	};
 };
 
 export default getState;
+
--- src/js/views/demo.js
+++ src/js/views/demo.js
@@ -1,43 +1,100 @@
 import React, { useState, useEffect, useContext } from "react";
-import { Link } from "react-router-dom";
-
+import { Link, } from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPen, faTrashCan, faPhone, faLocationDot, faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import { Context } from "../store/appContext";
-
 import "../../styles/demo.css";
+import { useNavigate } from "react-router-dom";
 
-export const Demo = () => {
-	const { store, actions } = useContext(Context);
+	export const Demo = () => {
+		const { store, actions } = useContext(Context);
+		console.log(store.contacts)
+		const navigate = useNavigate();
 
-	return (
-		<div className="container">
-			<ul className="list-group">
-				{store.demo.map((item, index) => {
-					return (
-						<li
-							key={index}
-							className="list-group-item d-flex justify-content-between"
-							style={{ background: item.background }}>
-							<Link to={"/single/" + index}>
-								<span>Link to: {item.title}</span>
+		const [modal, setModal] = useState(false); // controla la visibilidade del modal
+		const [contactToDelete, setContactToDelete]= useState(null);// el contacto que se va a eliminar
+
+		//Funcion que muestra el modal y guarda el contacto a eliminar
+		const deleteClick = (contact) => {
+			setContactToDelete(contact); //guarda el contacto a eliminar
+			setModal(true);//muestra el modal
+		};
+
+		//Funcion que confirma la eliminacion usando deleteContact de actions
+		const confirmDelete = () => {
+			if (contactToDelete) {
+				actions.deleteContact(contactToDelete.id); //usamos la accion deleteContact directamente
+			}
+			setModal(false); //cierra el modal
+		};
+		const closeModal =() => {
+			setModal(false);
+		};
+
+		console.log(store.contacts);  // Verifica si los contactos están llegando al store.
+		
+		return (
+				<div className="container">
+					<nav className="navbar  mb-3">
+						<div className="d-flex justify-content-end w-100">
+							<Link to="/">
+								<button className="btn btn-success">Add new contact</button>
 							</Link>
-							{// Conditional render example
-							// Check to see if the background is orange, if so, display the message
-							item.background === "orange" ? (
-								<p style={{ color: item.initial }}>
-									Check store/flux.js scroll to the actions to see the code
-								</p>
-							) : null}
-							<button className="btn btn-success" onClick={() => actions.changeColor(index, "orange")}>
-								Change Color
-							</button>
+						</div>
+					</nav>
+
+				<ul className="list-group">
+				{Array.isArray(store.contacts) && store.contacts.length > 0 ? (
+					store.contacts.map((item, index) => {
+						return (
+							<li key={index} className="list-group-item d-flex justify-content-between ">
+								<div className="d-flex align-items-center">
+									<img src="https://picsum.photos/id/64/150/150" 	alt="Contact" className="img-circle"/>
+									<div className="datos-del-contacto ml-3">
+										<h3>{item.name}</h3> 
+										<p>
+											<FontAwesomeIcon icon={faLocationDot} style={{color: "#646464", marginRight: "10px"}} />
+											{item.address}
+											<br/>
+											<FontAwesomeIcon icon={faPhone} flip="horizontal" style={{color: "#646464", marginRight: "10px"}} />
+											{item.phone}
+											<br/>
+											<FontAwesomeIcon icon={faEnvelope} style={{color: "#646464", marginRight: "10px"}} />
+											{item.email}
+										</p>
+									</div>
+								</div>
+								<div className="iconos">
+								<FontAwesomeIcon icon={faPen} onClick={()=> navigate(`/edit/${item.id}`)} className="m-3"/>
+								<FontAwesomeIcon icon={faTrashCan} onClick={()=>deleteClick(item.id)} className="m-3"/>
+								</div>
 						</li>
-					);
-				})}
-			</ul>
-			<br />
-			<Link to="/">
-				<button className="btn btn-primary">Back home</button>
-			</Link>
-		</div>
-	);
-};
+						);
+					})
+						
+				) : (<p>No contacts found.</p> // Muestra un mensaje si no hay contactos
+				)}
+					</ul>
+
+					{modal && (
+					<div className="modal" tabindex="-1">
+						<div className="modal-dialog">
+							<div className="modal-content">
+							<div className="modal-header">
+								<h5 className="modal-title">Modal title</h5>
+								<button type="button" className="btn-close" onClick={closeModal} data-bs-dismiss="modal" aria-label="Close"></button>
+							</div>
+							<div className="modal-body">
+								<p>Modal body text goes here.</p>
+							</div>
+							<div className="modal-footer">
+								<button type="button" className="btn btn-secondary" onClick={closeModal} data-bs-dismiss="modal">Close</button>
+								<button type="button" className="btn btn-primary" onClick={confirmDelete}>Save changes</button>
+							</div>
+							</div>
+						</div>
+						</div>
+					)}			
+				</div>
+			);
+		};
